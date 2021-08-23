1ec638f3f359675d76128d793dd3c2fe102e50a9
Alex <aszhukov91@gmail.com>
2021-08-23 18:35:52 +0300

    Добавлена модель GradientBoostingClassifier, answers_test, Merge.ipynb

---
+++ Prediction.py
@@ -0,0 +1,63 @@
+import dask.dataframe as dd
+import pandas as pd
+import pickle
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class FeatureSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        assert isinstance(X, pd.DataFrame)
+
+        return self
+
+    def transform(self, X):
+        assert isinstance(X, pd.DataFrame)
+
+        return X[self.columns]
+
+
+def merge_data(df, features):
+    data_merged = pd.merge(df, features, on='id')
+
+    duplicated_data = data_merged[data_merged['id'].duplicated(keep=False)].sort_values(by='id')
+    duplicated_data['time_diff'] = abs(duplicated_data['buy_time_x'] - duplicated_data['buy_time_y'])
+    duplicated_data = duplicated_data.sort_values(by=['Unnamed: 0_x', 'time_diff'])
+
+    before_clean = duplicated_data.index.tolist()
+    after_clean = duplicated_data.drop_duplicates(subset=['Unnamed: 0_x', 'id'], keep='first').index.tolist()
+    to_delete = list(set(before_clean) - set(after_clean))
+
+    data_merged.drop(to_delete, axis=0, inplace=True)
+    data_merged.drop(columns=['Unnamed: 0_y'], inplace=True)
+
+    del duplicated_data
+    return data_merged
+
+
+TEST_PATH = 'data_test.csv'
+FEATURES_PATH = 'features.csv'
+
+df_test = pd.read_csv(TEST_PATH)
+
+df_features = dd.read_csv(FEATURES_PATH, sep="\t")
+
+features_test = df_features.loc[df_features['id'].isin(df_test['id'])].compute()
+
+del df_features
+
+merged_test = merge_data(df_test, features_test)
+
+del features_test
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+predictions = model.predict_proba(merged_test)
+
+df_test['predictions'] = predictions[:, 1]
+df_test.drop(columns=['Unnamed: 0'], inplace=True)
+
+df_test.to_csv('answers_test.csv')
